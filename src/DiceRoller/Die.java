546e24ba0ae489f9d6a0a3c2b179cf921130f043
TugboatsGonWild <wesley.mcmillen@blackburn.edu>
2020-01-23 13:44:20 -0600

    This is where it all started...

--- src/DiceRoller/Die.java
+++ src/DiceRoller/Die.java
@@ -7,15 +7,8 @@ package emptyjavaproject;
 
 /**
  *
- * @author paul.kline
+ * @author wesley.mcmillen
  */
-public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Die {
     
 }
---
+++ src/DiceRoller/Runner.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package emptyjavaproject;
+import java.util.Scanner;
+/**PROJECTION...3 Hours
+ *
+ * @author paul.kline
+ */
+public class Runner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       
+    }
+    
+}
